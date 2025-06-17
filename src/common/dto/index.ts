85de30bca3c82da8148c73a45eb0a7016d8a2a88
dangminhchau80@gmail.com <dangminhchau80@gmail.com>
2025-06-17 11:28:02 +0700

    sf

---
+++ src/common/dto/index.ts
@@ -0,0 +1,7 @@
+// Common DTOs
+export { BaseResponseDto } from './base-response.dto';
+export {
+  PaginatedResponseDto,
+  PaginationMetaDto,
+} from './paginated-response.dto';
+export { UploadResponseDto } from './upload-response.dto';
