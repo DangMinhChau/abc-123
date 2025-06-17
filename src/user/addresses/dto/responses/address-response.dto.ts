b5a8955f8c781fb96811aa3eb8d49301e2db4706
dangminhchau80@gmail.com <dangminhchau80@gmail.com>
2025-06-17 18:37:06 +0700

    dsafdsa

--- src/user/addresses/dto/responses/address-response.dto.ts
+++ src/user/addresses/dto/responses/address-response.dto.ts
@@ -59,6 +59,46 @@ export class AddressResponseDto {
   isDefault: boolean;
 
   @ApiProperty({
+    description: 'GHN Province ID',
+    example: 202,
+    required: false,
+  })
+  @Expose()
+  ghnProvinceId?: number;
+
+  @ApiProperty({
+    description: 'GHN District ID',
+    example: 1442,
+    required: false,
+  })
+  @Expose()
+  ghnDistrictId?: number;
+
+  @ApiProperty({
+    description: 'GHN Ward Code',
+    example: '21211',
+    required: false,
+  })
+  @Expose()
+  ghnWardCode?: string;
+
+  @ApiProperty({
+    description: 'GHN Province Code',
+    example: 'HCM',
+    required: false,
+  })
+  @Expose()
+  ghnProvinceCode?: string;
+
+  @ApiProperty({
+    description: 'GHN District Code',
+    example: '1442',
+    required: false,
+  })
+  @Expose()
+  ghnDistrictCode?: string;
+
+  @ApiProperty({
     description: 'Creation timestamp',
     example: '2024-01-01T00:00:00.000Z',
   })
